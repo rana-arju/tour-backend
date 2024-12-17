2507aad358d3d5286bdbaec9878c9526dc56c198
rana-arju <arjurana20@gmail.com>
2024-12-17 12:06:23 +0600

    tour project: user, booking and tour API completed

---
+++ src/module/user/user.router.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response, Router } from 'express'
+import { userController } from './user.controller'
+import { UserValidation } from './userValidation'
+
+const userRouter = Router()
+
+userRouter.post('/create-user', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        console.log({ body: req.body });
+        const parsedBody = await UserValidation.userValidationSchema.parseAsync(req.body)
+        req.body = parsedBody
+        console.log({ parsedBody });
+        next()
+    } catch (error) {
+        next(error)
+    }
+}, userController.createUser)
+userRouter.get('/:userId', userController.getSingleUser)
+userRouter.put('/:userId', userController.updateUser)
+userRouter.delete('/:userId', userController.deleteUser)
+userRouter.get('/', userController.getUser)
+
+export default userRouter
